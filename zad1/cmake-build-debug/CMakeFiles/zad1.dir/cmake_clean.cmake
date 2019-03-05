689331a1fa65359421b39b6ba35f1b542cc3ed52
Pawel Maczuga <pmaczuga97@gmail.com>
2019-03-05 17:03:20 +0100

    Initail commit

---
+++ zad1/cmake-build-debug/CMakeFiles/zad1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zad1.dir/main.c.obj"
+  "zad1.pdb"
+  "zad1.exe"
+  "zad1.exe.manifest"
+  "libzad1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zad1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
